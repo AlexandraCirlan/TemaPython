ead8584d2bb5720b6bbe13bb183137d957c12b3c
AlexandraCirlan <125569697+AlexandraCirlan@users.noreply.github.com>
2024-12-18 16:42:28 +0200

    Update functii_test.py

--- functii_test.py
+++ functii_test.py
@@ -7,19 +7,19 @@ class TestEmployeeManager:
         Manager.mgr_count = 0
 
     def test_employee_creation(self):
-        emp = Employee("Alice", 4000)
-        assert emp.name == "Alice"
+        emp = Employee("Andreea", 4000)
+        assert emp.name == "Andreea"
         assert emp.salary == 4000
         assert Employee.empCount == 1
 
     def test_manager_creation(self):
-        mgr = Manager("John Doe", 5000, "Development")
-        assert mgr.name == "John Doe"
+        mgr = Manager("Alexandra Cirlan", 5000, "Development")
+        assert mgr.name == "Alexandra Cirlan"
         assert mgr.salary == 5000
         assert mgr.department == "Team_F34_Development"
         assert Manager.mgr_count == 1
 
     def test_employee_count(self):
-        emp1 = Employee("Alice", 4000)
-        emp2 = Employee("Bob", 5000)
+        emp1 = Employee("Andreea", 4000)
+        emp2 = Employee("Vlad", 5000)
         assert Employee.empCount == 2
